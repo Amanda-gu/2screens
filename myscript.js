43a8c37d8b63f2d207a5a15234a70e18be927ffb
Amanda-gu <60276244+Amanda-gu@users.noreply.github.com>
2020-03-06 13:58:16 -0500

    M

---
+++ myscript.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+	//js goes in here
+
+	
+$('.front1').on('click', function(){
+        $('.c01').append('Winter uses all the blues there are.  - Robert Francis');
+        $('.front1').hide('<div class="front1">1</div>');
+    });
+
+
+$('.front2').on('click', function(){
+        $('.c02').append('Roses are red, Violets are blue, Sugar is sweet, And so are you.');
+        $('.front2').hide('<div class="front2">2</div>');
+    });
+
+
+$('.front3').on('click', function(){
+        $('.c03').append('The Ocean has its silent cave, Deep, quiet, and alone - Nathaniel Hawthrone');
+        $('.front3').hide('<div class="front3">3</div>');
+    });
+
+
+
+$('.front4').on('click', function(){
+        $('.c04').append('Blue is Our Lady\'s color, White is Our Lord\'s. - Mary Elizabeth Coleridge');
+        $('.front4').hide('<div class="front4">4</div>');
+    });
+
+$('.front5').on('click', function(){
+        $('.c05').append('The water is the fish s element: Leave her but there, and she is well content. - John Bunyan');
+        $('.front5').hide('<div class="front5">5</div>');
+    });
+
+$('.front6').on('click', function(){
+        $('.c06').append('Blue color is everlastingly appointed by the deity to be a source of delight. - John Ruskin');
+        $('.front6').hide('<div class="front6">6</div>');
+    });
+
+$('.front7').on('click', function(){
+        $('.c07').append('There is no blue without yellow and without orange. - Vincent Van Gogh');
+        $('.front7').hide('<div class="front7">7</div>');
+    });
+
+$('.front8').on('click', function(){
+        $('.c08').append('Midnight blue is the only color that can ever compete with black. - Christian Dior');
+        $('.front8').hide('<div class="front8">8</div>');
+    });
+
+$('.front9').on('click', function(){
+        $('.c09').append('The sea! The sea! The open sea!, The blue, the fresh, the ever free! - Bryan Procter');
+        $('.front9').hide('<div class="front9">9</div>');
+    });
+
+$('.front10').on('click', function(){
+        $('.c010').append('Why make so much of fragmentary blue In here and there a bird, or butterfly, Or flower, or wearing-stone, or open eye, When heaven presents in sheets the solid hue? - Robert Frost');
+        $('.front10').hide('<div class="front10">10</div>');
+    });
+
+$('.front11').on('click', function(){
+        $('.c011').append('There are connoisseurs of blue just as there are connoisseurs of wine. - Sidonie Gabrielle Colette');
+        $('.front11').hide('<div class="front11">11</div>');
+    });
+
+$('.front12').on('click', function(){
+        $('.c012').append('Into my heart an air that kills, From yon far country blows: What are those blue remembered hills, What spires, what farms are those? - A. E. Housman');
+        $('.front12').hide('<div class="front12">12</div>');
+    });
+
+
+});
+
